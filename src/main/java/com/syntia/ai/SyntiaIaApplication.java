2ad5ce45d25326781c1821e5d1f9a859575a5251
Dani <danicanterog@gmail.com>
2026-03-13 10:57:12 +0100

    commit inicial, creación del proyecto springboot

---
+++ src/main/java/com/syntia/ai/SyntiaIaApplication.java
@@ -0,0 +1,13 @@
+package com.syntia.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyntiaIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyntiaIaApplication.class, args);
+	}
+
+}
