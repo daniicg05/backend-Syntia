d47f246e7e79e1001f7fb7e2f01932703cfb24a6
Dani <danicanterog@gmail.com>
2026-03-18 13:48:09 +0100

    creación y adaptación de UsuarioRepository

---
+++ src/main/java/com/syntia/ai/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.syntia.ai.repository;
+
+import com.syntia.ai.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
