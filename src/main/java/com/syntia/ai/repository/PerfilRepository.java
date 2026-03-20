0c6e80e7c05a8b19d60f632b345234abeacdff87
iuk1net <carlos_iuka88@hotmail.com>
2026-03-20 12:24:27 +0100

    Heredar CRUD

--- src/main/java/com/syntia/ai/repository/PerfilRepository.java
+++ src/main/java/com/syntia/ai/repository/PerfilRepository.java
@@ -1,11 +1,12 @@
 package com.syntia.ai.repository;
 
 import com.syntia.ai.model.Perfil;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface PerfilRepository {
+public interface PerfilRepository extends JpaRepository<Perfil, Long> {
     Optional<Perfil> findByUsuarioId(Long usuarioId);
 }
