d5ddbce05ba87ac2e56979e5ab3fd0e2874e1e9f
iuk1net <carlos_iuka88@hotmail.com>
2026-04-13 11:21:20 +0200

    fix

--- src/main/java/com/syntia/ai/repository/ConvocatoriaRepository.java
+++ src/main/java/com/syntia/ai/repository/ConvocatoriaRepository.java
@@ -1,6 +1,7 @@
 package com.syntia.ai.repository;
 
 import com.syntia.ai.model.Convocatoria;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
