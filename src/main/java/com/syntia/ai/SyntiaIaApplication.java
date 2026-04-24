3ace17e2ae13e19ead5866994f9f0e5503cd591a
kapanga25 <kapanga@hotmail.com>
2026-04-24 10:16:11 +0200

    feat/redesign-backend-2

--- src/main/java/com/syntia/ai/SyntiaIaApplication.java
+++ src/main/java/com/syntia/ai/SyntiaIaApplication.java
@@ -3,6 +3,7 @@ package com.syntia.ai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SyntiaIaApplication {
 
