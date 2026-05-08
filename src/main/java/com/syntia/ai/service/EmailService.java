748fac592ee157c5f9a925409d03a1eeac2d6493
iuk1net <carlos_iuka88@hotmail.com>
2026-05-08 10:24:03 +0200

    fix

--- src/main/java/com/syntia/ai/service/EmailService.java
+++ src/main/java/com/syntia/ai/service/EmailService.java
@@ -4,11 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import org.springframework.scheduling.annotation.Async;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
